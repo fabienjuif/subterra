1969d01553589bf71ae5b9e204a64373ec76404b
Fabien JUIF <fabien.juif@gmail.com>
2020-03-14 13:27:53 +0100

    :sparkles: borders (graphical only)

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import cn from "classnames";
 import classes from "./tile.module.scss";
 
-const Tile = ({
-  id,
-  start,
-  end,
-}) => {
+const Tile = ({ id, start, end, top, bottom, left, right }) => {
   return (
     <div
       className={cn(classes.tile, {
@@ -15,6 +11,12 @@ const Tile = ({
       })}
     >
       tile - {id}
+      <div>
+        {top || <div className={classes.top}>&nbsp;</div>}
+        {bottom || <div className={classes.bottom}>&nbsp;</div>}
+        {left || <div className={classes.left}>&nbsp;</div>}
+        {right || <div className={classes.right}>&nbsp;</div>}
+      </div>
     </div>
   );
 };
--- src/utils/tiles.js
+++ src/utils/tiles.js
@@ -2,12 +2,20 @@ export default {
     0: {
         id: 0,
         start: true,
+        top: true,
+        left: true,
+        bottom: true,
+        right: true,
     },
     1: {
         id: 1,
         end: true,
+        left: true,
+        bottom: true,
     },
     2: {
         id: 2,
+        left: true,
+        right: true,
     }
 }
