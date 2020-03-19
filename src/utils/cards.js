92cc2460c88e9f9efe5169354451d99a69fcfbf5
wru-talend <wruchaud@talend.com>
2020-03-20 00:03:58 +0100

    Add damage on card

--- src/utils/cards.js
+++ src/utils/cards.js
@@ -2,17 +2,21 @@ export default {
   0: {
     id: 0,
     type: 'end',
+    damage: 9,
   },
   1: {
     id: 1,
     type: 'water',
+    damage: 2,
   },
   2: {
     id: 2,
     type: 'gaz',
+    damage: 2,
   },
   3: {
     id: 3,
     type: 'shaking',
+    damage: 3,
   },
 }
