0f1c289083c661cc367df4539a97d1c8a2c5558d
Fabien JUIF <fabien.juif@gmail.com>
2020-03-30 11:37:22 +0200

    :sparkles: skill / protect

--- src/screens/prepare/getSkillDescription.js
+++ src/screens/prepare/getSkillDescription.js
@@ -3,6 +3,8 @@ export default (type) => {
   switch (type) {
     case 'experienced':
       return 'Add 1 to the rolled dice value'
+    case 'protect':
+      return 'Protect others players in the same tile'
     default:
       return ''
   }
