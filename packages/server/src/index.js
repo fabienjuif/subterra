4d2efc46f7f7949e84fbde59f10dbf8be750f972
Fabien JUIF <fabien.juif@gmail.com>
2020-03-31 09:42:17 +0200

    first connection via sockjs

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,5 +1,18 @@
 import { createEngine } from '@subterra/engine'
+import http from 'http'
+import sockjs from 'sockjs'
 
 const engine = createEngine()
 
-console.log(engine.getState())
+const echo = sockjs.createServer({})
+echo.on('connection', function (conn) {
+  conn.on('data', function (message) {
+    console.log({ message })
+    conn.write(message)
+  })
+  conn.on('close', function () {})
+})
+
+const server = http.createServer()
+echo.installHandlers(server, { prefix: '/game' })
+server.listen(9999, '0.0.0.0')
--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SockJS from 'sockjs-client'
 import './index.css'
 import App from './app'
 import * as serviceWorker from './serviceWorker'
+
+const sock = new SockJS('/game')
+sock.onopen = function () {
+  console.log('open')
+  sock.send('test')
+}
+
+sock.onmessage = function (e) {
+  console.log('message', e.data)
+  sock.close()
+}
+
+sock.onclose = function () {
+  console.log('close')
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
