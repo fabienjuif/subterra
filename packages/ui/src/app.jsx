25d22d670be9a05964fc966b2c25d5bc7111f9dc
Fabien JUIF <fabien.juif@gmail.com>
2020-04-04 20:41:16 +0200

    :ok_hand: remove useless code

--- packages/ui/src/app.jsx
+++ packages/ui/src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Prepare, Game, Lobby, Welcome } from './screens'
 import UserProvider from './userContext'
@@ -17,53 +17,6 @@ const App = () => {
       </BrowserRouter>
     </UserProvider>
   )
-  // const [mode, setMode] = useState()
-  // const [init, setInit] = useState()
-
-  // const connect = useCallback(
-  //   wrapSubmit(async (login) => {
-  //     const raw = await fetch('/auth', {
-  //       method: 'POST',
-  //       body: JSON.stringify(login),
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     })
-  //     if (raw.ok) {
-  //       const token = await raw.text()
-  //       setInit({ token })
-  //       setMode('online')
-  //     }
-  //   }),
-  //   [],
-  // )
-
-  // const onStart = useCallback((init) => {
-  //   setInit(init)
-  // }, [])
-
-  // if (!mode) {
-  //   return (
-  //     <div>
-  //       <form onSubmit={connect}>
-  //         <input name="username" type="text" placeholder="username" />
-  //         <input name="password" type="password" placeholder="password" />
-  //         <button type="submit">connect</button>
-  //       </form>
-  //       <button onClick={() => setMode('local')}>local</button>
-  //     </div>
-  //   )
-  // }
-
-  // if (init && init.token) return <Lobby {...init} />
-  // return null
-
-  // if (mode === 'local') {
-  //   if (!init) return <Prepare onStart={onStart} />
-  //   return <Game {...init} mode={mode} />
-  // }
-
-  // return <Game {...init} mode={mode} />
 }
 
 export default App
