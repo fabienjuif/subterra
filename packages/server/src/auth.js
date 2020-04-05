eb97061de8d7f60350e2d93bb6497ca33208ff0d
Fabien JUIF <fabien.juif@gmail.com>
2020-04-05 17:16:53 +0200

    :white_check_mark: api / auth

--- packages/server/src/auth.js
+++ packages/server/src/auth.js
@@ -1,19 +1,5 @@
-import { promisify } from 'util'
 import bodyParser from 'body-parser'
-import send from '@polka/send-type'
-import jwt from 'jsonwebtoken'
-
-const sign = promisify(jwt.sign)
-const verify = promisify(jwt.verify)
-
-let { PRIVATE_KEY } = process.env
-if (!PRIVATE_KEY) {
-  if (process.env.NODE_ENV === 'production') {
-    throw new Error('PRIVATE_KEY must be assigned for security reasons')
-  }
-
-  PRIVATE_KEY = 'devmode'
-}
+import { getToken, verifyAndGetInfos } from './resolvers'
 
 // TODO: database or auth0
 const USERS = [
@@ -30,50 +16,12 @@ const USERS = [
 export default (server, prefix) => {
   const withPrefix = (path) => `${prefix}${path}`
 
+  const context = { users: USERS }
+
   server
     .use(bodyParser.json())
-    .post(withPrefix('/'), async (req, res) => {
-      const { username, password } = req.body
-
-      const user = USERS.find(
-        (user) => user.username === username && user.password === password,
-      )
-      if (!user) {
-        send(res, 401)
-      } else {
-        const token = await sign({ userId: user.id }, PRIVATE_KEY)
-
-        send(res, 200, token)
-      }
-    })
-    .get(withPrefix('/'), async (req, res) => {
-      let token = req.query.token
-      if (!token) {
-        const authorization = req.headers['authorization']
-        if (authorization) {
-          const split = authorization.split('Bearer ')
-          if (split) token = split[1]
-        }
-      }
-
-      if (!token) {
-        console.trace(new Error('No token found'))
-        send(res, 401)
-        return
-      }
-
-      try {
-        const decoded = await verify(token, PRIVATE_KEY)
-
-        // filters what we send to the user
-        const { userId } = decoded
-        send(res, 200, { userId })
-      } catch (ex) {
-        console.trace(ex)
-        send(res, 401)
-        return
-      }
-    })
+    .post(withPrefix('/'), getToken(context))
+    .get(withPrefix('/'), verifyAndGetInfos(context))
 
   console.log('[auth] bounded to', prefix)
 }
