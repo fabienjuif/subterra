e69fec67f55544f1de83efc98c180ab183fcf42b
Fabien JUIF <fabien.juif@gmail.com>
2020-05-08 23:01:05 +0200

    :ok_hand: self review

--- packages/engine/src/utils/dices.js
+++ packages/engine/src/utils/dices.js
@@ -26,7 +26,6 @@ export const roll = (number, prevSeed) => {
 
 export const roll6 = (prevSeed) => roll(6, prevSeed)
 
-// TODO: test repartition
 export const getRandomInArray = (array, prevSeed) => {
   const { value, nextSeed } = roll(array.length, prevSeed)
   return { value: array[Math.round(value - 1)], nextSeed }
