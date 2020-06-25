fb3fe7dd470aef5a63607fc7a0cec1ca52e3901a
Fabien JUIF <fabien.juif@gmail.com>
2020-06-25 09:47:31 +0200

    :lipstick: actions in UI

--- packages/ui/src/components/action.jsx
+++ packages/ui/src/components/action.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import cn from 'classnames'
 import classes from './action.module.scss'
 
-const Action = ({ onClick, ...action }) => {
-  const isExcess = action.type === '@players>excess'
+const isExcess = (action) => action.type === '@players>excess'
 
+const mapName = (action) => {
+  const type = isExcess(action)
+    ? action.payload.actionOnSuccess.type
+    : action.type
+
+  if (type.startsWith('@players>')) return type.replace('@players>', '')
+  return type
+}
+
+const Action = ({ onClick, ...action }) => {
   return (
     <button
-      className={cn('action', classes.action, { [classes.excess]: isExcess })}
+      className={cn('action', classes.action, {
+        [classes.excess]: isExcess(action),
+      })}
       onClick={() => onClick && onClick(action)}
     >
-      {isExcess ? action.payload.actionOnSuccess.type : action.type}
+      {mapName(action)}
     </button>
   )
 }
